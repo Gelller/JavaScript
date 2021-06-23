2d8e3638b6757b6c8f4e480a2450f6d494684e76
Gelller <geller.ggs@gmail.com>
2021-06-23 11:16:15 +0300

    lesson-1

---
+++ script.js
@@ -0,0 +1,12 @@
+var temperatureC = prompt("Введите температуру по цельсию")
+
+var temperatureF = (9 / 5) * temperatureC + 32
+
+alert("Температура по Фаренгейту " + temperatureF + "F")
+
+var name1 = "Василий"
+var admin = name1.slice()
+alert(admin)
+
+var intOrString = 1000 + "108"
+alert(intOrString)
